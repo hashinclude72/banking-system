fee05041a180b699e2c446710277edc01c3bc607
tanmay agarwal <tanmayag8958@gmail.com>
2019-03-30 15:50:39 +0530

    UI improvement

--- GUI.py
+++ GUI.py
@@ -41,31 +41,57 @@ canvas.pack()
 background_label = tk.Label(root)
 background_label.place(relwidth=1, relheight=1)
 
+
+# MENU FRAME----------------------------------------------------------------------------------------------------
 frame = tk.Frame(root, bg='black', bd=5)
-frame.place(relx=0.5, rely=0.01, relwidth=0.95, relheight=0.15, anchor='n')
+frame.place(relx=0.5, rely=0.025, relwidth=0.95, relheight=0.15, anchor='n')
 
-# entry = tk.Entry(frame, font=40)
-# entry.place(relwidth=0.65, relheight=1)
-
-home = tk.Button(frame, text="HOME", font=40)
+home = tk.Button(frame, text="HOME", font=('Sans', '15', 'bold'), bd=10, bg='#413f63')
 home.place(relx=0, relheight=1, relwidth=0.2)
 
-button = tk.Button(frame, text="Accounts", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Accounts", font=('Sans', '15', 'bold'), bd=10, bg='#413f63', command=lambda: get_weather(entry.get()))
 button.place(relx=0.2, relheight=1, relwidth=0.2)
 
-home = tk.Button(frame, text="Fund Transfer", font=40)
+home = tk.Button(frame, text="Fund \nTransfer", font=('Sans', '15', 'bold'), bd=10, bg='#413f63')
 home.place(relx=0.4, relheight=1, relwidth=0.2)
 
-home = tk.Button(frame, text="Services", font=40)
+home = tk.Button(frame, text="Services", font=('Sans', '15', 'bold'), bd=10, bg='#413f63')
 home.place(relx=0.6, relheight=1, relwidth=0.2)
 
-home = tk.Button(frame, text="Logout", font=40)
+home = tk.Button(frame, text="Logout", font=('Sans', '15', 'bold'), bd=10, bg='#413f63')
 home.place(relx=0.8, relheight=1, relwidth=0.2)
+# MENU FRAME END----------------------------------------------------------------------------------------------------
 
-lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
-lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-label = tk.Label(lower_frame)
-label.place(relwidth=1, relheight=1)
+# BODY FRAME--------------------------------------------------------------------------------------------------------
+lower_frame = tk.Frame(root, bg='black', bd=10)
+lower_frame.place(relx=0.5, rely=0.2, relwidth=0.95, relheight=0.75, anchor='n')
+
+account_summary = tk.Button(lower_frame, text="Account Summary", font=('Sans', '15', 'bold'), bd=5, bg='#413f63')
+account_summary.place(relx=0, rely=0, relheight=0.15, relwidth=0.5)
+
+fund_transfer = tk.Button(lower_frame, text="Fund Transfer", font=('Sans', '15', 'bold'), bd=5, bg='#413f63')
+fund_transfer.place(relx=0.5, rely=0, relheight=0.15, relwidth=0.5)
+
+mini_statement = tk.Button(lower_frame, text="Mini Statement", font=('Sans', '15', 'bold'), bd=5, bg='#413f63')
+mini_statement.place(relx=0, rely=0.15, relheight=0.15, relwidth=0.5)
+
+mini_pdf = tk.Button(lower_frame, text="Download Mini\nStatement as PDF", font=('Sans', '15', 'bold'), bd=5, bg='#413f63')
+mini_pdf.place(relx=0.5, rely=0.15, relheight=0.15, relwidth=0.5)
+
+# label = tk.Label(lower_frame)
+# label.place(relwidth=1, relheight=1)
+# BODY FRAME END----------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
